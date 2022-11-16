4075e1b3bda2018582d0ff9cbc632b5888eae8d9
rhiana3b <rhiana3b@penguin>
2022-11-15 18:16:20 -0800

    final u4pp1 - LeapYear, MyMath, InputHelper the stuff actually works and everything

--- src/main/java/u4pp/InputHelper.java
+++ src/main/java/u4pp/InputHelper.java
@@ -3,12 +3,12 @@ package u4pp;
 import java.util.Scanner;
 
 public class InputHelper {
-    static String input;
+    String input;
+    Scanner myScanner;
+    
     //a constructor which takes in a scanner that is used to read the input
     InputHelper(Scanner scanner){
-        Scanner myScanner = scanner;
-        
-        input = myScanner.nextLine();
+        myScanner = scanner;
     }
 
     //takes in a prompt, asks the player the prompt. 
@@ -17,11 +17,17 @@ public class InputHelper {
     //Loops until proper input is given, at which point, it returns a `true` for yes and `false` for no.
     public boolean getYesNoInput(String prompt){
         System.out.println(prompt);
-        while (InputHelper.input != "y" || InputHelper.input != "Y" || InputHelper.input != "n" || InputHelper.input != "N"){
+        input = myScanner.nextLine();
+        while ((input.charAt(0) != 'y') && (input.charAt(0) != 'Y') && (input.charAt(0) != 'n') && (input.charAt(0) != 'N')){
+            System.out.println("input: " + input );
             System.out.println(prompt);
+            input = myScanner.nextLine();
         }
-        if (InputHelper.input .equals ("y") || InputHelper.input != "Y"){
+        if (input.charAt(0) == 'y'|| input.charAt(0) == 'Y'){
             return true;
+        }
+        else if (input.charAt(0) == 'n' || input.charAt(0) == 'N'){
+            return false;
         }
         return false;
     }
@@ -31,9 +37,12 @@ public class InputHelper {
     //The integer must be larger than or equal to `min`, and less than or equal to `max`.
     public int getIntegerInput(String prompt, int min, int max){
         System.out.println(prompt);
-        int num = Integer.parseInt(InputHelper.input);
+        input = myScanner.nextLine();
+        int num = Integer.parseInt(input);
         while (num < min || num > max){
             System.out.println(prompt);
+            input = myScanner.nextLine();
+            num = Integer.parseInt(input);
         }
         return num;
     }
--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner myScanner = new Scanner(System.in);
+        InputHelper myInputHelper = new InputHelper(myScanner);
+        myInputHelper.getYesNoInput("answer y/n");
+        myInputHelper.getIntegerInput("give me num", 1, 10);
     }
     
 }
